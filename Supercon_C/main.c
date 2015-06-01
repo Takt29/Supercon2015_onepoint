a51fcb794a1df58302d33c6b13d0de8e392eb03c
Takuto <takuto@Yoshidas-MacBook-Pro.local>
2015-06-01 18:23:00 +0900

    Initial Commit

---
+++ Supercon_C/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Supercon_C
+//
+//  Created by Takuto on 15/6/1.
+//  Copyright (c) 2015 Takuto. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(){
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
